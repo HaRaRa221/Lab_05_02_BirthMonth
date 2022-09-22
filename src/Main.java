dec14ee03b1edb9715cd5a0658a83ef7bbdfd9c0
Raymond Ha <Haro@mail.uc.edu>
2022-09-22 15:14:58 -0400

    Added birth Month

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int birthMonth ;
+
+        birthMonth = 15;
+
+        if(birthMonth >= 1 & birthMonth <= 12)
+        {
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        else if(birthMonth >= 13)
+        {
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
     }
 }
